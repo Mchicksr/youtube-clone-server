9dfbbe7c4685079f9cc20b84fa7a1a679a5c7e85
Mike hicks <mikehicks@Mikes-MacBook-Pro.local>
2021-10-18 16:37:18 -0400

    server updates

---
+++ config2.js
@@ -0,0 +1,5 @@
+
+ const config2 = {
+    CONNECTION_URL:`mongodb+srv:'mongodb+srv://codeCosmic:codeCosmic1234@cluster0.p2hqg.mongodb.net/youtube-clone?retryWrites=true&w=majority'`
+}
+export default config2
